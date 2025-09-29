eae4c142e3eed8eeb42d9e8b32c1963ca58059a4
Miguel Caballero <postaldigital.mc@gmail.com>
2025-09-29 17:20:47 -0500

    Laboratorio 5 Completo

---
+++ Laboratorio 5/Laboratorio 5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class PruebaVector
+{
+    private int[] sueldos; // Declaramos el vector
+
+    public void Cargar()
+    {
+        sueldos = new int[6]; // Inicializamos el vector con 5 elementos
+
+        for (int f = 0; f < sueldos.Length; f++)
+        {
+            Console.Write($"Ingrese sueldo del operario #{f + 1}: ");
+            string linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea); // Asignamos el sueldo al vector
+        }
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("\nLos 5 sueldos de los operarios son:");
+        for (int f = 0; f < sueldos.Length; f++)
+        {
+            Console.Write($"[{sueldos[f]}] ");
+        }
+        Console.ReadKey();
+    }
+
+    static void Main(string[] args)
+    {
+        PruebaVector pv = new PruebaVector();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
