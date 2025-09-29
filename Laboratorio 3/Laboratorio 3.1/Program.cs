8f70892211985c3a214116e6501b9d8d68672d54
Miguel Caballero <postaldigital.mc@gmail.com>
2025-09-29 07:48:41 -0500

    Laboratorio 3 Completo

---
+++ Laboratorio 3/Laboratorio 3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CalculosMatematicos
+{
+    public int Calcular(int a, int b)
+    {
+        return (a + b) * (a - b);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos calc = new CalculosMatematicos();
+        int resultado = calc.Calcular(a, b);
+
+        Console.WriteLine($"El resultado es: {resultado}");
+    }
+}
---
+++ Laboratorio 3/Laboratorio 3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class CalculosMatematicos
+{
+    public double CalculoArea(double radio)
+    {
+        return Math.PI * radio * radio;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el radio del círculo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        CalculosMatematicos calc = new CalculosMatematicos();
+        double area = calc.CalculoArea(radio);
+
+        Console.WriteLine($"El área del círculo es: {area}");
+    }
+}
