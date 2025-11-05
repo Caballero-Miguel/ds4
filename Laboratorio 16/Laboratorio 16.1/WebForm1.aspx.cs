37a8d88d41b8162d8cb3bb1d8407e92c9de27fa3
Miguel Caballero <postaldigital.mc@gmail.com>
2025-11-05 15:02:31 -0500

    Segunda parte del Laboratorio 16.1

--- Laboratorio 16/Laboratorio 16.1/WebForm1.aspx.cs
+++ Laboratorio 16/Laboratorio 16.1/WebForm1.aspx.cs
@@ -18,6 +18,7 @@ namespace Laboratorio_16._1
 
         }
 
+        //Botones Numerico 1
         protected void b1_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -30,6 +31,7 @@ namespace Laboratorio_16._1
 
         }
 
+        //Botones Numerico 2
         protected void b2_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -41,6 +43,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b2.Text;
         }
 
+        //Botones Numerico 3
         protected void b3_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -52,6 +55,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b3.Text;
         }
 
+        //Boton Suma
         protected void add_Click(object sender, EventArgs e)
         {
             a = Convert.ToInt32(t.Text);
@@ -60,6 +64,7 @@ namespace Laboratorio_16._1
             t.Text += b;
         }
 
+        //Botones Numerico 4
         protected void b4_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -71,6 +76,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b4.Text;
         }
 
+        // Botones Numerico 5
         protected void b5_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -82,6 +88,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b5.Text;
         }
 
+        // Botones Numerico 6
         protected void b6_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -93,6 +100,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b6.Text;
         }
 
+        // Boton Resta
         protected void sub_Click(object sender, EventArgs e)
         {
             a = Convert.ToInt32(t.Text);
@@ -101,6 +109,7 @@ namespace Laboratorio_16._1
             t.Text += b;
         }
 
+        // Botones Numerico 7
         protected void b7_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -112,6 +121,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b7.Text;
         }
 
+        // Botones Numerico 8
         protected void b8_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -123,6 +133,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b8.Text;
         }
 
+        // Botones Numerico 9
         protected void b9_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -134,6 +145,7 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b9.Text;
         }
 
+        // Boton Multiplicacion
         protected void mul_Click(object sender, EventArgs e)
         {
             a = Convert.ToInt32(t.Text);
@@ -142,6 +154,7 @@ namespace Laboratorio_16._1
             t.Text += b;
         }
 
+        // Botones Numerico 0
         protected void b0_Click(object sender, EventArgs e)
         {
             if ((t.Text == "+") || (t.Text == "-") || (t.Text == "*") || (t.Text == "/"))
@@ -153,11 +166,13 @@ namespace Laboratorio_16._1
                 t.Text = t.Text + b0.Text;
         }
 
+        // Boton Limpiar
         protected void clr_Click(object sender, EventArgs e)
         {
             t.Text = "";
         }
 
+        // Boton Igual
         protected void eql_Click(object sender, EventArgs e)
         {
             c = Convert.ToInt32(t.Text);
@@ -186,6 +201,7 @@ namespace Laboratorio_16._1
             }
         }
 
+        // Boton Division
         protected void div_Click(object sender, EventArgs e)
         {
             a = Convert.ToInt32(t.Text);
